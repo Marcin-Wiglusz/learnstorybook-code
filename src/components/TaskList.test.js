b9ff3380123c45809ba646a6dd66dbb73c737bea
Tom Coleman <tom@thesnail.org>
2018-03-27 18:23:25 +1100

    Made into a very basic redux app

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 
 it('renders when empty', () => {
   const div = document.createElement('div');
   const events = { onSnoozeTask: jest.fn(), onPinTask: jest.fn(), onArchiveTask: jest.fn() };
-  ReactDOM.render(<TaskList tasks={[]} {...events} />, div);
+  ReactDOM.render(<PureTaskList tasks={[]} {...events} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
